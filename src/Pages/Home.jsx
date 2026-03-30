e8a29c8c21289f034b3d4fe529d5b6d41fbad627
soheldev.codes <soheldev.codes@gmail.com>
2026-03-31 00:55:15 +0600

    Footer Design Implement done

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "../Components/Navbar/Navbar";
+import Footer from "../Components/Footer/Footer";
 
 const Home = () => {
   return (
     <>
       <Navbar />
+      <Footer />
     </>
   );
 };
